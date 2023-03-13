3304cfd5f1f13e3f7f18c919610ad9bbb8ee89b4
Paul Delifer <paul.delifer@ikmail.com>
2023-03-13 18:18:48 +0100

    fixed brand title color for hero section

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  constructor(private router:Router) { }
+export class NavbarComponent implements OnInit {
+  currentUrl!: string|undefined;
 
-  async onIconClick(){
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  }
+  async onIconClick() {
     console.log("icon clicked")
     console.log(localStorage.getItem('token'))
-    if(localStorage.getItem('token')){
+    if (localStorage.getItem('token')) {
       await this.router.navigate(['/dashboard'])
-    }else{
+    } else {
       await this.router.navigate(['/'])
     }
+  }
+
+  ngOnInit(): void {
+    this.currentUrl=this.router.url;
+    console.log(this.currentUrl)
   }
 }
