36a30d8085a9acc8d287fcd19390eb4791adcee2
Paul Delifer <paul.delifer@ikmail.com>
2023-03-17 20:27:21 +0100

    feat: added @ChildView and fixed border

--- src/app/modules/dashboard/project-view/project-view.component.ts
+++ src/app/modules/dashboard/project-view/project-view.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-project-view',
   templateUrl: './project-view.component.html',
   styleUrls: ['./project-view.component.scss']
 })
-export class ProjectViewComponent {
+export class ProjectViewComponent implements OnInit{
+  @ViewChild('png') png: ElementRef<HTMLImageElement> | undefined;
+  @ViewChild('mp4') mp4: ElementRef<HTMLVideoElement> | undefined;
+  @ViewChild('wav') wav: ElementRef<HTMLAudioElement> | undefined;
+  @ViewChild('txt') mp3: ElementRef<HTMLAudioElement> | undefined;
 
+  constructor() {}
+  ngOnInit(): void {}
 }
