457a67f50ac044fef4702fbf4bd457aad47aef8d
Paul Delifer <paul.delifer@yahoo.fr>
2023-04-04 20:02:46 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ShortVision_API:'http://localhost:8080/api'
+  ShortVision_API:'https://shortvisions.api.oups.net/'
   //ShortVision_API: 'shortvisions.api.oups.net',
 };
