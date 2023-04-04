b37b20450f8e242dc460a781203e8b673b74ebcc
Paul Delifer <paul.delifer@yahoo.fr>
2023-04-04 21:22:03 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   YoutubeApiKey: 'AIzaSyCMVI1k--USZfhLMN034NUE_DTFB9xv-6k',
-  ShortVision_API:'https://shortvisions.api.oups.net',
+  ShortVision_API:'https://shortvisions.api.oups.net/api',
 }
